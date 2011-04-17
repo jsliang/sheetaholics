7e0da64dba93f865d8c275f567735d7fdd2e8dec
Jui-Shan Liang <jsliang.tw@gmail.com>
2011-04-17 12:58:17 +0800

    dottedlined: Move import block (from reportlab.pdfbase import _fontdata_*) from dottedlined.py to generator.py

--- dottedlined/generator.py
+++ dottedlined/generator.py
@@ -5,6 +5,34 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 import sys
 
+# Explicitly import reportlab.pdfbase._fontdata_*
+# Uncomment these lines if you are running PyInstaller on Windows
+# Reference:
+#     http://www.python-forum.org/pythonforum/viewtopic.php?f=15&t=21922
+"""
+from reportlab.pdfbase import _fontdata_widths_courier
+from reportlab.pdfbase import _fontdata_widths_courierbold
+from reportlab.pdfbase import _fontdata_widths_courieroblique
+from reportlab.pdfbase import _fontdata_widths_courierboldoblique
+from reportlab.pdfbase import _fontdata_widths_helvetica
+from reportlab.pdfbase import _fontdata_widths_helveticabold
+from reportlab.pdfbase import _fontdata_widths_helveticaoblique
+from reportlab.pdfbase import _fontdata_widths_helveticaboldoblique
+from reportlab.pdfbase import _fontdata_widths_timesroman
+from reportlab.pdfbase import _fontdata_widths_timesbold
+from reportlab.pdfbase import _fontdata_widths_timesitalic
+from reportlab.pdfbase import _fontdata_widths_timesbolditalic
+from reportlab.pdfbase import _fontdata_widths_symbol
+from reportlab.pdfbase import _fontdata_widths_zapfdingbats
+from reportlab.pdfbase import _fontdata_enc_winansi
+from reportlab.pdfbase import _fontdata_enc_macroman
+from reportlab.pdfbase import _fontdata_enc_standard
+from reportlab.pdfbase import _fontdata_enc_symbol
+from reportlab.pdfbase import _fontdata_enc_zapfdingbats
+from reportlab.pdfbase import _fontdata_enc_pdfdoc
+from reportlab.pdfbase import _fontdata_enc_macexpert
+"""
+
 def genpdf(filename, config):
     if config == None:
         config = {}
